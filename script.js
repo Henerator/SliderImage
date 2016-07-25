20a4cf7b0ddaab16d41a160c339a7f101b4a4284
WhiteMan <whiteblacks@yandex.ru>
2016-07-25 17:31:25 +0300

    Small change in the script
    Trying to optimize

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 $(document).ready(function() {
 	function Slider(slider_div) {
-		var $slider = $(slider_div);
-		var slides;
+		var $slider;
+		var $slides;
 		var currentSlide = 0;
 		var previousSlide = -1;
-		var timer = null;
+		
 		var slideChangeDelay = 3000;
+		var timer = null;
 
 		function nextSlide() {
 			if (previousSlide >= 0) {
-				$(slides[previousSlide]).css("top", "-324px");
+				$($slides[previousSlide]).css("top", "-324px");
 			}
 
 			previousSlide = currentSlide;
 			currentSlide++;
-			if (currentSlide >= slides.length) {
+			if (currentSlide >= $slides.length) {
 				currentSlide = 0;
 			}
 
-			$(slides[previousSlide]).css("top", "324px");
-			$(slides[previousSlide]).css("opacity", "0.0");
+			$($slides[previousSlide]).css("top", "324px");
+			$($slides[previousSlide]).css("opacity", "0.0");
 
-			$(slides[currentSlide]).css("top", "0px");
-			$(slides[currentSlide]).css("opacity", "1.0");
+			$($slides[currentSlide]).css("top", "0px");
+			$($slides[currentSlide]).css("opacity", "1.0");
 			timer = setTimeout(nextSlide, slideChangeDelay);
 		}
 
 		function init() {
+			$slider = $(slider_div);
 			$slider.mouseover(function() {
 				if (timer != null) clearTimeout(timer);
 			});
@@ -35,15 +37,15 @@ $(document).ready(function() {
 			});
 
 			//	find all slides
-			slides = $slider.find($("li.slider_image"));
+			$slides = $slider.find($("li.slider_image"));
 			//	hide
-			slides.each(function() {
+			$slides.each(function() {
 				$(this).css("top", "-324px");
 				$(this).css("opacity", "0.0");
 			});
 			//	show first slide
-			$(slides[0]).css("top", "0px");
-			$(slides[0]).css("opacity", "1.0");
+			$($slides[0]).css("top", "0px");
+			$($slides[0]).css("opacity", "1.0");
 
 			timer = setTimeout(nextSlide, slideChangeDelay);
 		}
